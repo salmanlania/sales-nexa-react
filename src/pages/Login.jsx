b0acbdad83c508f7b351475fe7528435a3b1dbbc
indenta-ai <shahbazabdullah81@gmail.com>
2024-09-06 11:35:40 +0500

    installation update accroding to netlify web deploy

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div style={{display : 'flex' , justifyContent : 'center' , marginTop : '30%'}}>Login</div>
+    <div style={{display : 'flex' , justifyContent : 'center' , marginTop : '31%'}}>Login</div>
   )
 }
